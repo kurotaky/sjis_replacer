aebf18737dca90a832889571e6ea32087c09c929
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2015-10-29 10:00:53 +0900

    add replace method

--- spec/sjis_replacer_spec.rb
+++ spec/sjis_replacer_spec.rb
@@ -4,4 +4,10 @@ describe SJISReplacer do
   it 'has a version number' do
     expect(SJISReplacer::VERSION).to eq '0.1.0'
   end
+
+  describe '#replace' do
+    it 'replace non sjis safe characters' do
+      expect(SJISReplacer.replace('①')).to eq('1')
+    end
+  end
 end
